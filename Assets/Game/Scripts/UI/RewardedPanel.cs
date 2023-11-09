7c503b3306bf7026b58361bf571d4289f84e08b7
Bohdan Kovalenko <Ko.b@dcjudgement.com>
2023-11-09 13:25:03 +0200

    - Підключив рекламу

---
+++ Assets/Game/Scripts/UI/RewardedPanel.cs
@@ -0,0 +1,51 @@
+using Tools.UnityAdsService.Scripts;
+using UI.Panels;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class RewardedPanel : Panel
+    {
+        [SerializeField] private Button _button= null;
+
+        private void Update()
+        {
+            UpdateState();
+        }
+
+        private void UpdateState()
+        {
+            bool isAdReady = CheckIfAdIsReady();
+
+            _button.interactable = isAdReady;
+        }
+
+        private bool CheckIfAdIsReady()
+        {
+            return UnityAdsService.Instance.IsAvailableShow;
+        }
+
+        private void ShowAd()
+        {
+            var listener = UnityAdsService.Instance.ShowRewardedAd();
+
+            listener.OnShowCompleteAds += GiveReward;
+        }
+
+        private void GiveReward()
+        {
+            Wallet.AddMoney(100);
+            
+            Hide();
+        }
+        
+        public void TryShowRewardedAd()
+        {
+            if (CheckIfAdIsReady())
+            {
+                ShowAd();
+            }
+        }
+    }
+}
